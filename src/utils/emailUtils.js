11b37f1f6500b546d8fa87cb2427d16f27c450a3
Mohamed-Agwa <moh.agwa1@gmail.com>
2026-07-29 08:41:28 +0300

    Verification and Password Email Template

--- src/utils/emailUtils.js
+++ src/utils/emailUtils.js
@@ -1,6 +1,5 @@
 const mailConfig = require("../config/mail");
 const logger = require("./logger");
-
 const BREVO_API_URL = "https://api.brevo.com/v3/smtp/email";
 
 const sendEmail = async ({ to, subject, html }) => {
