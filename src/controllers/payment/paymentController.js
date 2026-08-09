ee69c224bc3e044a1b352cf67f45d178f0ed8761
Mohamed-Agwa <moh.agwa1@gmail.com>
2026-08-09 14:22:10 +0300

    Paymob Added -7

--- src/controllers/payment/paymentController.js
+++ src/controllers/payment/paymentController.js
@@ -175,7 +175,7 @@ exports.createPaymobOrder = asyncHandler(async (req, res) => {
         }
 
         logger.info(`Enriched Items: ${enrichedItems[0]}`);
-        
+
 
         order = await orderService.createOrder(
             {
